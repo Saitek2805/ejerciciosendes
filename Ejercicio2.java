ec2d6416e021f8c5ad08f30127550a4099055e73
JmiguelC26 <145494230+JmiguelC26@users.noreply.github.com>
2023-10-03 20:47:17 +0200

    Ejercicio2.java

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -7,13 +7,14 @@ public class Ejercicio2 {
 	public static void main(String[] args) {
 		int distancia,velocidad;//Valores dados a la distancia y a la velocidad(distancia,velocidad)
 		float tiempo;
-		float hora=3600;
+		
 		Scanner teclado = new Scanner(System.in);
 		System.out.println("Dime los kilometros que hay hasta el destino");
 		distancia=teclado.nextInt();
+		
 		System.out.println("Dime la velocidad promedia que lleva el tren");
 		velocidad=teclado.nextInt();
-		tiempo= (distancia*velocidad)/hora;
+		tiempo= distancia/velocidad;
 		
 		System.out.println("El tiempo que se tardará en llegar al sitio es:"+ tiempo + "h");
 
